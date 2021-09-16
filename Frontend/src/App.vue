9371fad5b237049b6cd9ce153864ef382815a574
Benjamin Stauf <stauf.b03@htlwienwest.at>
2021-09-16 13:48:47 +0200

    v1.1

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,31 +1,37 @@
 <template>
-	<v-app>
-		<v-app-bar color="#08CCCB" app>
-			<!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+  <v-app>
+    <v-app-bar color="#08CCCB" app>
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
 
-			<v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-			<v-btn icon>
-				<router-link :to="{ name: 'Shop' }" class="text-decoration-none"> <v-icon class="black--text">mdi-home</v-icon></router-link>
-			</v-btn>
+      <v-btn icon>
+        <router-link :to="{ name: 'Shop' }" class="text-decoration-none">
+          <v-icon class="black--text">mdi-home</v-icon></router-link
+        >
+      </v-btn>
 
-			<v-btn icon>
-				<router-link :to="{ name: 'Account' }" class="text-decoration-none"> <v-icon class="black--text">mdi-account</v-icon> </router-link>
-			</v-btn>
+      <v-btn icon>
+        <router-link :to="{ name: 'Account' }" class="text-decoration-none">
+          <v-icon class="black--text">mdi-account</v-icon>
+        </router-link>
+      </v-btn>
 
-			<v-btn icon>
-				<router-link :to="{ name: 'Basket' }" class="text-decoration-none"><v-icon class="black--text">mdi-cart-outline</v-icon></router-link>
-			</v-btn>
-		</v-app-bar>
+      <v-btn icon>
+        <router-link :to="{ name: 'Basket' }" class="text-decoration-none"
+          ><v-icon class="black--text">mdi-cart-outline</v-icon></router-link
+        >
+      </v-btn>
+    </v-app-bar>
 
-		<v-main>
-			<router-view />
-		</v-main>
-	</v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
-	name: 'app',
+  name: 'app',
 };
 </script>
