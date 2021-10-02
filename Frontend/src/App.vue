e821e937213964e84a5b2583df37deab4c47ee28
LukasSemler <semler.l04@htlwienwest.at>
2021-10-02 18:39:10 +0200

    Search funktioniert (keine Ahnung wie)

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,74 +1,74 @@
 <template>
-  <!--Navbar-->
-  <v-app>
-    <v-app-bar color="cyan accent-2" app>
-      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
-      <v-spacer></v-spacer>
+	<!--Navbar-->
+	<v-app>
+		<v-app-bar color="cyan accent-2" app>
+			<!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+			<v-spacer></v-spacer>
 
-      <v-btn icon>
-        <router-link :to="{ name: 'Shop' }" class="text-decoration-none">
-          <v-icon class="black--text">mdi-home</v-icon>
-        </router-link>
-      </v-btn>
+			<v-btn icon>
+				<router-link :to="{ name: 'Shop' }" class="text-decoration-none">
+					<v-icon class="black--text">mdi-home</v-icon>
+				</router-link>
+			</v-btn>
 
-      <v-btn icon>
-        <router-link :to="{ name: 'Account' }" class="text-decoration-none">
-          <v-icon class="black--text">mdi-account</v-icon>
-        </router-link>
-      </v-btn>
+			<v-btn icon>
+				<router-link :to="{ name: 'Account' }" class="text-decoration-none">
+					<v-icon class="black--text">mdi-account</v-icon>
+				</router-link>
+			</v-btn>
 
-      <v-btn icon>
-        <router-link :to="{ name: 'AboutUs' }" class="text-decoration-none">
-          <v-icon class="black--text">mdi-account-group</v-icon>
-        </router-link>
-      </v-btn>
+			<v-btn icon>
+				<router-link :to="{ name: 'AboutUs' }" class="text-decoration-none">
+					<v-icon class="black--text">mdi-account-group</v-icon>
+				</router-link>
+			</v-btn>
 
-      <v-btn icon>
-        <router-link :to="{ name: 'Basket' }" class="text-decoration-none">
-          <v-badge :value="WarenkorbAnzahl" :content="WarenkorbAnzahl" :v-if="WarenkorbAnzahl > 0">
-            <v-icon class="black--text">mdi-cart-outline</v-icon>
-          </v-badge>
-        </router-link>
-      </v-btn>
-    </v-app-bar>
+			<v-btn icon>
+				<router-link :to="{ name: 'Basket' }" class="text-decoration-none">
+					<v-badge :value="WarenkorbAnzahl" :content="WarenkorbAnzahl" :v-if="WarenkorbAnzahl > 0">
+						<v-icon class="black--text">mdi-cart-outline</v-icon>
+					</v-badge>
+				</router-link>
+			</v-btn>
+		</v-app-bar>
 
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+		<v-main>
+			<router-view />
+		</v-main>
+	</v-app>
 </template>
 
 <script>
 import eventBus from './eventbus';
 
 export default {
-  name: 'app',
-  data() {
-    return {
-      WarenkorbAnzahl: 0,
-    };
-  },
+	name: 'app',
+	data() {
+		return {
+			WarenkorbAnzahl: 0,
+		};
+	},
 
-  created() {
-    try {
-      this.$store.dispatch('LoadProducts');
-    } catch (err) {
-      console.log(`Beim laden der Produkte ist ein Fehler aufgetreten! Error: ${err}`);
-    }
+	created() {
+		try {
+			this.$store.dispatch('LoadProducts');
+		} catch (err) {
+			console.log(`Beim laden der Produkte ist ein Fehler aufgetreten! Error: ${err}`);
+		}
 
-    try {
-      this.$store.dispatch('LoadCategories');
-    } catch (err) {
-      console.log(`Beim laden der Kategorien ist ein Fehler aufgetreten! Error: ${err}`);
-    }
+		try {
+			this.$store.dispatch('LoadCategories');
+		} catch (err) {
+			console.log(`Beim laden der Kategorien ist ein Fehler aufgetreten! Error: ${err}`);
+		}
 
-    //Warenkorb aktuallisieren mit LocalStorage
-    this.$store.dispatch('ReloadWarenkorbFromLocalStorage');
+		//Warenkorb aktuallisieren mit LocalStorage
+		this.$store.dispatch('ReloadWarenkorbFromLocalStorage');
 
-    eventBus.$on('UpdateLocalStorage', () => {
-      console.log();
-      this.WarenkorbAnzahl = this.$store.state.warenkorb.length;
-    });
-  },
+		eventBus.$on('UpdateLocalStorage', () => {
+			console.log();
+			this.WarenkorbAnzahl = this.$store.state.warenkorb.length;
+		});
+	},
 };
 </script>
--- Frontend/src/views/Shop.vue
+++ Frontend/src/views/Shop.vue
@@ -1,135 +1,154 @@
 <template>
-  <div>
-    <!--Überschrift-->
-    <h1 class="black--text center text-center pt-4">
-      <span class="black--text display-3">u</span
-      ><span class="cyan--text accent-2 display-3 ">SHOP</span>
-    </h1>
+	<div>
+		<!--Überschrift-->
+		<h1 class="black--text center text-center pt-4">
+			<span class="black--text display-3">u</span
+			><span class="cyan--text accent-2 display-3 ">SHOP</span>
+		</h1>
 
-    <!--Webseiten-Beschreibung-->
-    <p class="black--text pl-6 pr-6 pt-6">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt atque sequi, dignissimos veniam
-      quo illo maxime, iste culpa non consectetur recusandae. Delectus fugit pariatur quasi
-      accusamus cupiditate ipsam nostrum autem laborum incidunt sed temporibus et vel odit eius
-      laboriosam quo mollitia, eaque laudantium officiis architecto voluptatibus recusandae? Nostrum
-      repellendus mollitia corrupti, esse maiores porro cum adipisci fugit perspiciatis consectetur.
-      Accusantium cupiditate voluptatum error debitis nostrum et ipsum quas sequi, animi numquam
-      optio culpa voluptate possimus impedit laborum minima molestias iusto vero tempora nesciunt
-      eius accusamus. Nostrum, dolore, optio, nisi aspernatur quos porro rerum iure repellendus
-      voluptate architecto eius illo ipsam.
-    </p>
+		<!--Webseiten-Beschreibung-->
+		<p class="black--text pl-6 pr-6 pt-6">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt atque sequi, dignissimos veniam
+			quo illo maxime, iste culpa non consectetur recusandae. Delectus fugit pariatur quasi
+			accusamus cupiditate ipsam nostrum autem laborum incidunt sed temporibus et vel odit eius
+			laboriosam quo mollitia, eaque laudantium officiis architecto voluptatibus recusandae? Nostrum
+			repellendus mollitia corrupti, esse maiores porro cum adipisci fugit perspiciatis consectetur.
+			Accusantium cupiditate voluptatum error debitis nostrum et ipsum quas sequi, animi numquam
+			optio culpa voluptate possimus impedit laborum minima molestias iusto vero tempora nesciunt
+			eius accusamus. Nostrum, dolore, optio, nisi aspernatur quos porro rerum iure repellendus
+			voluptate architecto eius illo ipsam.
+		</p>
 
-    <!-- Filter -->
-    <v-icon class="black--text ml-6" @click="ToggleFilter()">mdi-filter</v-icon>
-    <v-container align="center" v-if="filterShow">
-      <v-row justify="center">
-        <ul v-for="Kategorie in $store.state.Kategorien" :key="Kategorie.ID">
-          <v-checkbox
-            color="cyan darken-2 "
-            v-bind:label="Kategorie.Name"
-            v-bind:value="Kategorie.Name"
-            v-model="selected"
-          ></v-checkbox>
-        </ul>
-      </v-row>
-    </v-container>
+		<!-- Filter Kategorie-->
+		<v-icon class="black--text ml-6" @click="ToggleFilter()">mdi-filter</v-icon>
+		<v-container align="center" v-if="filterShow">
+			<v-row justify="center">
+				<ul v-for="Kategorie in $store.state.Kategorien" :key="Kategorie.ID">
+					<v-checkbox
+						color="cyan darken-2 "
+						v-bind:label="Kategorie.Name"
+						v-bind:value="Kategorie.Name"
+						v-model="selected"
+					></v-checkbox>
+				</ul>
+			</v-row>
+		</v-container>
 
-    <!--Produkte-->
-    <!-- Wenn kein Filter gesetzt ist, werden alle angezeigt -->
-    <div v-if="selected.length == 0">
-      <v-container class="d-flex flex-wrap justify-space-around justify-center align-center">
-        <div v-for="produkt in changeProducts" :key="produkt.ProduktID">
-          <router-link
-            :to="{
-              name: 'Product_Detail',
-              params: {
-                ID: produkt.ProduktID,
-                Name: produkt.Name,
-                Preis: produkt.Preis,
-                Kurzbeschreibung: produkt.Kurzbeschreibung,
-                Kategorie: produkt.Kategorie,
-                Link3D: produkt.Link3D,
-                Bewertung: produkt.Bewertung,
-              },
-            }"
-          >
-            <Card :_productObj="produkt" />
-          </router-link>
-        </div>
-        <!-- </div> -->
-      </v-container>
-    </div>
+		<!-- Filter search -->
+		<v-icon class="black--text ml-6" @click="ToggleSearch()">mdi-magnify</v-icon>
+		<v-container class="justify-center" v-if="searchShow">
+			<v-col cols="4" class="pt-4 justify-center">
+				<v-row class="justify-center">
+					<v-text-field label="Suchen" solo v-model="pattern"></v-text-field>
+				</v-row>
+			</v-col>
+		</v-container>
 
-    <!-- Wenn ein Filter gesetzt ist, werden nur diese Angezeigt -->
-    <div v-else>
-      <v-container class="d-flex flex-wrap justify-space-around justify-center">
-        <div v-for="produkt in changeProducts" :key="produkt.ID">
-          <!-- Filter -->
-          <div v-if="selected.includes(produkt.Kategorie)">
-            <router-link
-              :to="{
-                name: 'Product_Detail',
-                params: {
-                  ID: produkt.ProduktID,
-                  Name: produkt.Name,
-                  Preis: produkt.Preis,
-                  Kurzbeschreibung: produkt.Kurzbeschreibung,
-                  Kategorie: produkt.Kategorie,
-                  Link3D: produkt.Link3D,
-                  Bewertung: produkt.Bewertung,
-                },
-              }"
-            >
-              <Card :_productObj="produkt" />
-            </router-link>
-          </div>
-        </div>
-      </v-container>
-    </div>
-  </div>
+		<!--Produkte anzeigen-->
+		<div>
+			<v-container class="d-flex flex-wrap justify-space-around justify-center align-center">
+				<div v-for="produkt in filterProducts" :key="produkt.ProduktID">
+					<router-link
+						:to="{
+							name: 'Product_Detail',
+							params: {
+								ID: produkt.ProduktID,
+								Name: produkt.Name,
+								Preis: produkt.Preis,
+								Kurzbeschreibung: produkt.Kurzbeschreibung,
+								Kategorie: produkt.Kategorie,
+								Link3D: produkt.Link3D,
+								Bewertung: produkt.Bewertung,
+							},
+						}"
+					>
+						<Card :_productObj="produkt" />
+					</router-link>
+				</div>
+			</v-container>
+		</div>
+	</div>
 </template>
 
 <script>
 import Card from '../components/Card.vue';
 
 export default {
-  name: 'Shop',
+	name: 'Shop',
 
-  components: {
-    Card,
-  },
-  data() {
-    return {
-      name: 'Test',
-      selected: [],
-      filterShow: false,
-    };
-  },
-  methods: {
-    ToggleFilter() {
-      this.filterShow = !this.filterShow;
-    },
-  },
-  created() {
-    //Letztes Item aus dem LocalStorage löschen
-    localStorage.removeItem('LastObj');
-  },
-  computed: {
-    changeProducts() {
-      let selected = this.selected;
-      let returnArray;
-      if (selected.length == 0) {
-        returnArray = this.$store.state.produkte;
-      } else {
-        returnArray = this.$store.state.produkte.filter((element) => {
-          if (selected.includes(element.Kategorie)) {
-            return element;
+	components: {
+		Card,
+	},
+	data() {
+		return {
+			name: 'Test',
+			selected: [],
+			filterShow: false,
+			searchShow: false,
+			pattern: '',
+		};
+	},
+	methods: {
+		ToggleFilter() {
+			this.filterShow = !this.filterShow;
+		},
+		ToggleSearch() {
+			this.searchShow = !this.searchShow;
+		},
+	},
+	created() {
+		//Letztes Item aus dem LocalStorage löschen
+		localStorage.removeItem('LastObj');
+	},
+	computed: {
+		changeProducts() {
+			let selected = this.selected;
+			let returnArray;
+			if (selected.length == 0) {
+				returnArray = this.$store.state.produkte;
+			} else {
+				returnArray = this.$store.state.produkte.filter((element) => {
+					if (selected.includes(element.Kategorie)) {
+						return element;
+					}
+				});
+			}
+
+			return returnArray;
+		},
+		filterProducts() {
+			let array = this.$store.state.produkte;
+			let pattern = this.pattern;
+			let filter = new RegExp(pattern, 'i');
+			let newArray = [];
+			let returnArray = [];
+			let echtesReturnArray = [];
+
+			if (pattern.length != 0) {
+				for (const iterator of array) {
+					newArray.push(iterator.Name);
+				}
+
+				newArray.forEach((elem) => {
+					if (elem.match(filter)) {
+						returnArray.push(elem);
+					}
+				});
+
+				 array.filter((elem) => {
+					if (returnArray.includes(elem.Name)) {
+            console.log("True");
+            echtesReturnArray.push(elem)
           }
-        });
-      }
-
-      return returnArray;
-    },
-  },
+				});
+				console.log(returnArray);
+				console.log(echtesReturnArray);
+        return echtesReturnArray
+			} else {
+				console.log('else');
+				return array;
+			}
+		},
+	},
 };
 </script>
