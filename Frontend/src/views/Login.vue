ae3e01d5938b3407c677ad0138aa657af6efc788
LukasSemler <semler.l04@htlwienwest.at>
2021-10-05 18:56:30 +0200

    Login fertig (funktion noch nicht 100%)

--- Frontend/src/views/Login.vue
+++ Frontend/src/views/Login.vue
@@ -2,26 +2,38 @@
 	<div>
 		<h1 class="black--text text-center">Login</h1>
 
-		<v-form>
-			<!-- <v-container>
+		<v-form ref="submit" lazy-validation>
+			<v-container class="mt-12">
 				<v-row class="justify-center">
-					<v-col cols="12" sm="12">
-						<v-text-field v-model="email" label="Email" clearable></v-text-field>
+					<v-col cols="6">
+						<v-row class="justify-center">
+							<v-col cols="12" sm="12">
+								<v-text-field
+									v-model="email"
+									label="Email"
+									clearable
+									required
+									:rules="[rules.email]"
+								></v-text-field>
+							</v-col>
+							<v-col cols="12" sm="12">
+								<v-text-field
+									v-model="password"
+									:append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+									:rules="rules.required"
+									:type="show1 ? 'text' : 'password'"
+									name="input-10-1"
+									label="Passwort"
+									@click:append="show1 = !show1"
+								></v-text-field>
+							</v-col>
+							<v-btn type="submit" @click="submit">Submit</v-btn>
+						</v-row>
+						<br>
+						<router-link class="black--text text-decoration-none" :to="{name: 'Register'}"><p>Zurück zum Registrieren</p></router-link>
 					</v-col>
-					<v-col cols="12" sm="12">
-						<v-text-field
-							v-model="password"
-							:append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-							:rules="rules.required"
-							:type="show1 ? 'text' : 'password'"
-							name="input-10-1"
-							label="Passwort"
-							@click:append="show1 = !show1"
-						></v-text-field>
-					</v-col>
-					<v-btn>Submit</v-btn>
 				</v-row>
-			</v-container> -->
+			</v-container>
 		</v-form>
 	</div>
 </template>
@@ -31,14 +43,23 @@ export default {
 	data() {
 		return {
 			email: '',
-			passwort: '',
+			password: '',
 			show1: false,
 			rules: {
-				required: (value) => !!value || 'Required.',
+				required: [(val) => (val || '').length > 0 || 'This field is required'],
+				email: (value) => {
+					const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<script>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+					return pattern.test(value) || 'Invalid e-mail.';
+				},
 			},
 		};
 	},
-	created() {},
+	computed: {},
+	methods: {
+		submit() {
+			console.log('Submit');
+		},
+	},
 };
 </script>
 
--- Frontend/src/views/Shop.vue
+++ Frontend/src/views/Shop.vue
@@ -1,137 +1,137 @@
 <template>
-  <div>
-    <!--Überschrift-->
-    <h1 class="black--text center text-center pt-4">
-      <span class="black--text display-3">u</span
-      ><span class="cyan--text accent-2 display-3 ">SHOP</span>
-    </h1>
+	<div>
+		<!--Überschrift-->
+		<h1 class="black--text center text-center pt-4">
+			<span class="black--text display-3">u</span
+			><span class="cyan--text accent-2 display-3 ">SHOP</span>
+		</h1>
 
-    <!--Webseiten-Beschreibung-->
-    <p class="black--text pl-6 pr-6 pt-6">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt atque sequi, dignissimos veniam
-      quo illo maxime, iste culpa non consectetur recusandae. Delectus fugit pariatur quasi
-      accusamus cupiditate ipsam nostrum autem laborum incidunt sed temporibus et vel odit eius
-      laboriosam quo mollitia, eaque laudantium officiis architecto voluptatibus recusandae? Nostrum
-      repellendus mollitia corrupti, esse maiores porro cum adipisci fugit perspiciatis consectetur.
-      Accusantium cupiditate voluptatum error debitis nostrum et ipsum quas sequi, animi numquam
-      optio culpa voluptate possimus impedit laborum minima molestias iusto vero tempora nesciunt
-      eius accusamus. Nostrum, dolore, optio, nisi aspernatur quos porro rerum iure repellendus
-      voluptate architecto eius illo ipsam.
-    </p>
+		<!--Webseiten-Beschreibung-->
+		<p class="black--text pl-6 pr-6 pt-6">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt atque sequi, dignissimos veniam
+			quo illo maxime, iste culpa non consectetur recusandae. Delectus fugit pariatur quasi
+			accusamus cupiditate ipsam nostrum autem laborum incidunt sed temporibus et vel odit eius
+			laboriosam quo mollitia, eaque laudantium officiis architecto voluptatibus recusandae? Nostrum
+			repellendus mollitia corrupti, esse maiores porro cum adipisci fugit perspiciatis consectetur.
+			Accusantium cupiditate voluptatum error debitis nostrum et ipsum quas sequi, animi numquam
+			optio culpa voluptate possimus impedit laborum minima molestias iusto vero tempora nesciunt
+			eius accusamus. Nostrum, dolore, optio, nisi aspernatur quos porro rerum iure repellendus
+			voluptate architecto eius illo ipsam.
+		</p>
 
-    <!-- Filter Kategorie-->
-    <v-icon class="black--text ml-6" @click="filterShow = !filterShow">mdi-filter</v-icon>
-    <v-container align="center" v-if="filterShow">
-      <v-row justify="center">
-        <ul v-for="Kategorie in $store.state.Kategorien" :key="Kategorie.ID">
-          <v-checkbox
-            color="cyan darken-2 "
-            v-bind:label="Kategorie.Name"
-            v-bind:value="Kategorie.Name"
-            v-model="selected"
-          ></v-checkbox>
-        </ul>
-      </v-row>
-    </v-container>
+		<!-- Filter Kategorie-->
+		<v-icon class="black--text ml-6" @click="filterShow = !filterShow">mdi-filter</v-icon>
+		<v-container align="center" v-if="filterShow">
+			<v-row justify="center">
+				<ul v-for="Kategorie in $store.state.Kategorien" :key="Kategorie.ID">
+					<v-checkbox
+						color="cyan darken-2 "
+						v-bind:label="Kategorie.Name"
+						v-bind:value="Kategorie.Name"
+						v-model="selected"
+					></v-checkbox>
+				</ul>
+			</v-row>
+		</v-container>
 
-    <!-- Filter search -->
-    <v-icon class="black--text ml-6" @click="searchShow = !searchShow">mdi-magnify</v-icon>
-    <v-container class="justify-center" v-if="searchShow">
-      <v-col cols="4" class="pt-4 justify-center">
-        <v-row class="justify-center">
-          <v-text-field label="Suchen" solo v-model="pattern"></v-text-field>
-        </v-row>
-      </v-col>
-    </v-container>
+		<!-- Filter search -->
+		<v-icon class="black--text ml-6" @click="searchShow = !searchShow">mdi-magnify</v-icon>
+		<v-container class="justify-center" v-if="searchShow">
+			<v-col cols="4" class="pt-4 justify-center">
+				<v-row class="justify-center">
+					<v-text-field label="Suchen" solo v-model="pattern"></v-text-field>
+				</v-row>
+			</v-col>
+		</v-container>
 
-    <!--Produkte anzeigen-->
-    <div>
-      <v-container class="d-flex flex-wrap justify-space-around justify-center align-center">
-        <div v-for="produkt in setFilter" :key="produkt.ProduktID">
-          <router-link
-            :to="{
-              name: 'Product_Detail',
-              params: {
-                ID: produkt.ProduktID,
-                Name: produkt.Name,
-                Preis: produkt.Preis,
-                Kurzbeschreibung: produkt.Kurzbeschreibung,
-                Kategorie: produkt.Kategorie,
-                Link3D: produkt.Link3D,
-                Bewertung: produkt.Bewertung,
-              },
-            }"
-          >
-            <Card :_productObj="produkt" />
-          </router-link>
-        </div>
-      </v-container>
-    </div>
-  </div>
+		<!--Produkte anzeigen-->
+		<div>
+			<v-container class="d-flex flex-wrap justify-space-around justify-center align-center">
+				<div v-for="produkt in setFilter" :key="produkt.ProduktID">
+					<router-link
+						:to="{
+							name: 'Product_Detail',
+							params: {
+								ID: produkt.ProduktID,
+								Name: produkt.Name,
+								Preis: produkt.Preis,
+								Kurzbeschreibung: produkt.Kurzbeschreibung,
+								Kategorie: produkt.Kategorie,
+								Link3D: produkt.Link3D,
+								Bewertung: produkt.Bewertung,
+							},
+						}"
+					>
+						<Card :_productObj="produkt" />
+					</router-link>
+				</div>
+			</v-container>
+		</div>
+	</div>
 </template>
 
 <script>
 import Card from '../components/Card.vue';
 
 export default {
-  name: 'Shop',
+	name: 'Shop',
 
-  components: {
-    Card,
-  },
-  data() {
-    return {
-      name: 'Test',
-      selected: [],
-      filterShow: false,
-      searchShow: false,
-      pattern: '',
-    };
-  },
-  methods: {
-    searchFilter() {},
-    kategorieFilter() {},
-  },
-  created() {
-    //Letztes Item aus dem LocalStorage löschen
-    localStorage.removeItem('LastObj');
-  },
+	components: {
+		Card,
+	},
+	data() {
+		return {
+			name: 'Test',
+			selected: [],
+			filterShow: false,
+			searchShow: false,
+			pattern: '',
+		};
+	},
+	methods: {
+		searchFilter() {},
+		kategorieFilter() {},
+	},
+	created() {
+		//Letztes Item aus dem LocalStorage löschen
+		localStorage.removeItem('LastObj');
+	},
 
-  computed: {
-    setFilter() {
-      //Variablen für Kategorie Filter:
-      let selected = this.selected;
-      //Variablen für Search Filter
-      let ProduktArray = this.$store.state.produkte;
-      let pattern = this.pattern;
+	computed: {
+		setFilter() {
+			//Variablen für Kategorie Filter:
+			let selected = this.selected;
+			//Variablen für Search Filter
+			let ProduktArray = this.$store.state.produkte;
+			let pattern = this.pattern;
 
-      //Filter-Vorgang für Schaulustige(Lukas)
-      //   return ProduktArray.filter((Produkt) => {
-      //     //Wenn keine Kategorie gewählt wurde
-      //     if (selected.length == 0) {
-      //       return Produkt.Name.toLowerCase().includes(pattern.toLowerCase());
-      //     }
-      //     //Wenn eine Kategorie gewählt wurde
-      //     else {
-      //       if (
-      //         Produkt.Name.toLowerCase().includes(pattern.toLowerCase()) &&
-      //         selected.includes(Produkt.Kategorie)
-      //       ) {
-      //         return Produkt;
-      //       }
-      //     }
-      //   });
+			//Filter-Vorgang für Schaulustige
+			//   return ProduktArray.filter((Produkt) => {
+			//     //Wenn keine Kategorie gewählt wurde
+			//     if (selected.length == 0) {
+			//       return Produkt.Name.toLowerCase().includes(pattern.toLowerCase());
+			//     }
+			//     //Wenn eine Kategorie gewählt wurde
+			//     else {
+			//       if (
+			//         Produkt.Name.toLowerCase().includes(pattern.toLowerCase()) &&
+			//         selected.includes(Produkt.Kategorie)
+			//       ) {
+			//         return Produkt;
+			//       }
+			//     }
+			//   });
 
-      //Filter-Vorgang
-      return ProduktArray.filter((Produkt) =>
-        selected.length == 0
-          ? Produkt.Name.toLowerCase().includes(pattern.toLowerCase())
-          : Produkt.Name.toLowerCase().includes(pattern.toLowerCase()) &&
-            selected.includes(Produkt.Kategorie)
-          ? Produkt
-          : console.log('Nix'),
-      );
-    },
-  },
+			//Filter-Vorgang
+			return ProduktArray.filter((Produkt) =>
+				selected.length == 0
+					? Produkt.Name.toLowerCase().includes(pattern.toLowerCase())
+					: Produkt.Name.toLowerCase().includes(pattern.toLowerCase()) &&
+					  selected.includes(Produkt.Kategorie)
+					? Produkt
+					: console.log('Nix'),
+			);
+		},
+	},
 };
 </script>
