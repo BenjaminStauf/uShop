2a2ea67c67fcf1ea99d090e39404d425546f7299
Lukas Semler <semler.l04@htlwienwest.at>
2021-10-12 11:43:22 +0200

    Change Anzahl

--- Frontend/src/views/Basket.vue
+++ Frontend/src/views/Basket.vue
@@ -1,112 +1,129 @@
 <template>
-  <div v-if="this.$store.state.warenkorb.length">
-    <h1 class="text-center pt-6 pb-6">Produkte im warenkorb</h1>
-    <v-container class="d-flex flex-wrap justify-space-around">
-      <div v-for="produkt in $store.state.warenkorb" :key="produkt.Name">
-        <v-card outlined max-width="500px" class="ma-4">
-          <v-list-item three-line>
-            <v-list-item-content>
-              <v-list-item-title class="text-h5 mb-1">
-                {{ produkt.ProduktID }} {{ produkt.Name }} {{ produkt.Link3D }}
-              </v-list-item-title>
-              <v-list-item-subtitle>{{ produkt.Kurzbeschreibung }}</v-list-item-subtitle>
-            </v-list-item-content>
+	<div v-if="this.$store.state.warenkorb.length">
+		<h1 class="text-center pt-6 pb-6">Produkte im Warenkorb</h1>
+		<v-container class="d-flex flex-wrap justify-space-around">
+			<div v-for="produkt in $store.state.warenkorb" :key="produkt.Name">
+				<v-card outlined max-width="500px" class="ma-4">
+					<v-list-item three-line>
+						<v-list-item-content>
+							<v-list-item-title class="text-h5 mb-1">
+								{{ produkt.ProduktID }} {{ produkt.Name }} {{ produkt.Link3D }}
+							</v-list-item-title>
+							<v-list-item-subtitle>{{ produkt.Kurzbeschreibung }}</v-list-item-subtitle>
+							<v-col cols="4">
+								<v-select
+									v-model="produkt.Anzahl"
+									:items="selectAnzahl"
+									@change="UpdatePreis"
+								></v-select>
+							</v-col>
+						</v-list-item-content>
 
-            <v-list-item-avatar tile size="100" color="grey"></v-list-item-avatar>
-          </v-list-item>
+						<v-list-item-avatar tile size="100" color="grey"></v-list-item-avatar>
+					</v-list-item>
 
-          <v-card-actions>
-            <router-link
-              :to="{
-                name: 'Product_Detail',
-                params: {
-                  ID: produkt.ID,
-                  Name: produkt.Name,
-                  Preis: produkt.Preis,
-                  Kurzbeschreibung: produkt.Kurzbeschreibung,
-                  Kategorie: produkt.Kategorie,
-                  Link3D: produkt.Link3D,
-                  Bewertung: produkt.Bewertung,
-                },
-              }"
-              class="text-decoration-none"
-            >
-              <v-btn outlined text class="lime accent-3">
-                Zum produkt
-              </v-btn>
-            </router-link>
-            <v-btn outlined text class="red accent-3 ml-3" @click="warenkorbRemove(produkt)">
-              Produkt löschen
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </div>
-    </v-container>
-    <v-divider></v-divider>
+					<v-card-actions>
+						<router-link
+							:to="{
+								name: 'Product_Detail',
+								params: {
+									ID: produkt.ID,
+									Name: produkt.Name,
+									Preis: produkt.Preis,
+									Kurzbeschreibung: produkt.Kurzbeschreibung,
+									Kategorie: produkt.Kategorie,
+									Link3D: produkt.Link3D,
+									Bewertung: produkt.Bewertung,
+									Anzahl: produkt.Anzahl,
+								},
+							}"
+							class="text-decoration-none"
+						>
+							<v-btn outlined text class="lime accent-3">
+								Zum produkt
+							</v-btn>
+						</router-link>
+						<v-btn outlined text class="red accent-3 ml-3" @click="warenkorbRemove(produkt)">
+							Produkt löschen
+						</v-btn>
+					</v-card-actions>
+				</v-card>
+			</div>
+		</v-container>
+		<v-divider></v-divider>
 
-    <v-container class="d-flex flex-wrap justify-space-around pt-4">
-      <h3>Summe aller Produkte: {{ Summe_warenkorb }}€</h3>
-      <v-btn @click="Bezahlen" outlined text class="lime accent-3">
-        Bezahlen
-      </v-btn>
-    </v-container>
-  </div>
-  <div v-else>
-    <h1 class="text-center pt-6">Es sind noch keine Produkte vorhanden</h1>
-    <v-snackbar v-model="snackbar" :timeout="1000">
-      Der Artikel wurde erfolgreich gelöscht
-    </v-snackbar>
-  </div>
+		<v-container class="d-flex flex-wrap justify-space-around pt-4">
+			<h3>Summe aller Produkte: {{ Summe_warenkorb }}€</h3>
+			<v-btn @click="Bezahlen" outlined text class="lime accent-3">
+				Bezahlen
+			</v-btn>
+		</v-container>
+	</div>
+	<div v-else>
+		<h1 class="text-center pt-6">Es sind noch keine Produkte vorhanden</h1>
+		<v-snackbar v-model="snackbar" :timeout="1000">
+			Der Artikel wurde erfolgreich gelöscht
+		</v-snackbar>
+	</div>
 </template>
 
 <script>
 import eventBus from '../eventbus';
 export default {
-  data() {
-    return {
-      Summe_warenkorb: 0,
-      AnzahlImwarenkorb: this.$store.state.warenkorb.length,
-      snackbar: false,
-    };
-  },
+	data() {
+		return {
+			Summe_warenkorb: 0,
+			AnzahlImwarenkorb: this.$store.state.warenkorb.length,
+			snackbar: false,
+			selectAnzahl: [],
+		};
+	},
 
-  created() {
-    //Preis updaten
-    this.UpdatePreis();
-  },
+	created() {
+		for (let index = 1; index < 100; index++) {
+			this.selectAnzahl.push(index);
+		}
 
-  methods: {
-    //Preisupdaten-Funktion
-    UpdatePreis: function() {
-      //Preis resetten
-      this.Summe_warenkorb = 0;
-      //Preis neu definieren
-      this.$store.state.warenkorb.forEach((element) => {
-        this.Summe_warenkorb += element.Preis;
-      });
-    },
-    //Lösch-Funktion
-    warenkorbRemove: function(produktZuLöschen) {
-      this.$store.state.warenkorb = this.$store.state.warenkorb.filter((element) => {
-        if (element.ID != produktZuLöschen.ID) {
-          return true;
-        }
-      });
+		//Preis updaten
+		this.UpdatePreis();
+	},
 
-      //Warenkorb mit LocalStorage stimmig machen
-      localStorage.removeItem('WarenkorbStorage');
-      localStorage.setItem('WarenkorbStorage', JSON.stringify(this.$store.state.warenkorb));
+	methods: {
+		//Preisupdaten-Funktion
+		UpdatePreis: function() {
+			localStorage.removeItem('WarenkorbStorage');
+			localStorage.setItem('WarenkorbStorage', JSON.stringify(this.$store.state.warenkorb));
 
-      //Preis updaten:
-      this.UpdatePreis();
+			console.log('Change');
+			//Preis resetten
+			this.Summe_warenkorb = 0;
+			//Preis neu definieren
+			this.$store.state.warenkorb.forEach((element) => {
+				this.Summe_warenkorb += element.Preis * element.Anzahl;
+			});
+		},
+		//Lösch-Funktion
+		warenkorbRemove: function(produktZuLöschen) {
+			this.$store.state.warenkorb = this.$store.state.warenkorb.filter((element) => {
+				if (element.ID != produktZuLöschen.ID) {
+					return true;
+				}
+			});
 
-      //Update Badge
-      eventBus.$emit('UpdateLocalStorage');
-      this.snackbar = true;
-    },
-    Bezahlen() {
-      alert('Sie werden zum Bezahlen weitergeleitet');
-    },
-  },
+			//Warenkorb mit LocalStorage stimmig machen
+			localStorage.removeItem('WarenkorbStorage');
+			localStorage.setItem('WarenkorbStorage', JSON.stringify(this.$store.state.warenkorb));
+
+			//Preis updaten:
+			this.UpdatePreis();
+
+			//Update Badge
+			eventBus.$emit('UpdateLocalStorage');
+			this.snackbar = true;
+		},
+		Bezahlen() {
+			alert('Sie werden zum Bezahlen weitergeleitet');
+		},
+	},
 };
 </script>
--- Frontend/src/views/Product_Detail.vue
+++ Frontend/src/views/Product_Detail.vue
@@ -1,119 +1,136 @@
 <template>
-  <div>
-    <v-row>
-      <v-col cols="8">
-        <v-row class="justify-center mt-6">
-          <v-img
-            class=""
-            max-height="500px"
-            max-width="500px"
-            src="https://ctc-wenzel.de/wp-content/uploads/Foto-Platzhalter.jpg"
-          ></v-img>
-        </v-row>
-      </v-col>
-      <v-divider vertical></v-divider>
-      <v-col cols="4">
-        <h2 class="text-center mt-6">{{ Name }}</h2>
-        <br />
-        <h3 class="ml-3">Kategorie: {{ Kategorie }}</h3>
-        <br />
-        <h3 class="ml-3">{{ Preis }}€</h3>
-        <br />
-        <h3 class="ml-3 mr-3">{{ Kurzbeschreibung }}</h3>
-        <br />
-        <v-rating
-          empty-icon="mdi-star-outline"
-          full-icon="mdi-star"
-          half-icon="$mdiStarHalfFull"
-          length="5"
-          size="32"
-          :value="Bewertung"
-          readonly
-          color="warning"
-          background-color="grey"
-        ></v-rating>
-        <br />
-        <br />
+	<div>
+		<v-row>
+			<v-col cols="8">
+				<v-row class="justify-center mt-6">
+					<v-img
+						class=""
+						max-height="500px"
+						max-width="500px"
+						src="https://ctc-wenzel.de/wp-content/uploads/Foto-Platzhalter.jpg"
+					></v-img>
+				</v-row>
+			</v-col>
+			<v-divider vertical></v-divider>
+			<v-col cols="4">
+				<h2 class="text-center mt-6">{{ Name }}</h2>
+				<br />
+				<h3 class="ml-3">Kategorie: {{ Kategorie }}</h3>
+				<br />
+				<h3 class="ml-3">{{ Preis }}€</h3>
+				<br />
+				<h3 class="ml-3 mr-3">{{ Kurzbeschreibung }}</h3>
+				<br />
+				<v-col cols="4">
+					<v-select :items="anzahl" label="Anzahl" dense solo v-model="selectAnzahl"></v-select>
+				</v-col>
+				<v-rating
+					empty-icon="mdi-star-outline"
+					full-icon="mdi-star"
+					half-icon="$mdiStarHalfFull"
+					length="5"
+					size="32"
+					:value="Bewertung"
+					readonly
+					color="warning"
+					background-color="grey"
+				></v-rating>
+				<br />
+				<br />
+				<v-btn outlined text class="lime accent-3" @click="AddBasket">
+					Zum Warenkorb hinzufügen<v-icon right>mdi-cart-outline</v-icon>
+				</v-btn>
+			</v-col>
+		</v-row>
+		<br />
+		<br />
 
-        <v-btn outlined text class="lime accent-3" @click="AddBasket">
-          Zum Warenkorb hinzufügen<v-icon right>mdi-cart-outline</v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
-    <br />
-    <br />
-
-    <v-divider></v-divider>
-    <router-link :to="{ name: 'Shop' }" class="text-decoration-none">
-      <v-btn outlined text class="lime accent-3">Zurück zu den Produkten</v-btn>
-    </router-link>
-    <br />
-    <br />
-    <v-snackbar v-model="snackbar" :timeout="timeout">{{ text }}</v-snackbar>
-  </div>
+		<v-divider></v-divider>
+		<router-link :to="{ name: 'Shop' }" class="text-decoration-none">
+			<v-btn outlined text class="lime accent-3">Zurück zu den Produkten</v-btn>
+		</router-link>
+		<br />
+		<br />
+		<v-snackbar v-model="snackbar" :timeout="timeout">{{ text }}</v-snackbar>
+	</div>
 </template>
 
 <script>
-import eventBus from '../eventbus';
-
 export default {
-  data() {
-    return {
-      ID: this.$route.params.ID,
-      Name: this.$route.params.Name,
-      Preis: this.$route.params.Preis,
-      Kurzbeschreibung: this.$route.params.Kurzbeschreibung,
-      Kategorie: this.$route.params.Kategorie,
-      Link3D: this.$route.params.Link3D,
-      Bewertung: this.$route.params.Bewertung,
-      snackbar: false,
-      text: `Der Artikel wurde zum Warenkorb hinzugefügt`,
-      timeout: 1000,
-    };
-  },
-  methods: {
-    AddBasket() {
-      //Item dem Warenkorb hinzufügen
-      let ThisProduct = {
-        ID: this.ID,
-        Name: this.Name,
-        Preis: this.Preis,
-        Kurzbeschreibung: this.Kurzbeschreibung,
-        Kategorie: this.Kategorie,
-        Link3D: this.Link3D,
-        Bewertung: this.Bewertung,
-      };
-      this.$store.dispatch('PushInWarenkorb', ThisProduct);
+	data() {
+		return {
+			ID: this.$route.params.ID,
+			Name: this.$route.params.Name,
+			Preis: this.$route.params.Preis,
+			Kurzbeschreibung: this.$route.params.Kurzbeschreibung,
+			Kategorie: this.$route.params.Kategorie,
+			Link3D: this.$route.params.Link3D,
+			Bewertung: this.$route.params.Bewertung,
+			AnzahlBasket: this.$route.params.Anzahl,
+			snackbar: false,
+			text: `Der Artikel wurde zum Warenkorb hinzugefügt`,
+			timeout: 1000,
+			anzahl: [],
+			selectAnzahl: 0,
+		};
+	},
+	methods: {
+		AddBasket() {
+			if (this.selectAnzahl != 0) {
+				//Item dem Warenkorb hinzufügen
+				let ThisProduct = {
+					ID: this.ID,
+					Name: this.Name,
+					Preis: this.Preis,
+					Kurzbeschreibung: this.Kurzbeschreibung,
+					Kategorie: this.Kategorie,
+					Link3D: this.Link3D,
+					Bewertung: this.Bewertung,
+					Anzahl: this.selectAnzahl,
+				};
+				this.$store.dispatch('PushInWarenkorb', ThisProduct);
 
-      //Warenkorb Bestätigungsnachricht anzeigen
-      this.snackbar = true;
-    },
-  },
-  mounted() {
-    //Bei einem Seiten-Refresh wird das Letzte aufgerufene Objekt geladen
-    if (localStorage.getItem('LastObj') == null) {
-      //Objekt Speichern
-      let aktuellObjekt = {
-        ID: this.ID,
-        Name: this.Name,
-        Preis: this.Preis,
-        Kurzbeschreibung: this.Kurzbeschreibung,
-        Kategorie: this.Kategorie,
-        Link3D: this.Link3D,
-        Bewertung: this.Bewertung,
-      };
-      localStorage.setItem('LastObj', JSON.stringify(aktuellObjekt));
-    } else {
-      //Objekt Laden
-      let erg = JSON.parse(localStorage.getItem('LastObj'));
-      this.Name = erg.Name;
-      this.Preis = erg.Preis;
-      this.Kategorie = erg.Kategorie;
-      this.Kurzbeschreibung = erg.Kurzbeschreibung;
-      this.Link3D = erg.Link3D;
-      this.Bewertung = erg.Bewertung;
-    }
-  },
+				//Warenkorb Bestätigungsnachricht anzeigen
+				this.snackbar = true;
+			} else alert('Null');
+		},
+	},
+	mounted() {
+		console.log(this.AnzahlBasket);
+		//Anzahl Select befüllen
+		for (let index = 1; index < 100; index++) {
+			this.anzahl.push(index);
+		}
+
+		//Anzahl aus Warenkorb bekommen
+		if (this.AnzahlBasket > 0) {
+			this.selectAnzahl = this.AnzahlBasket;
+		}
+
+		//Bei einem Seiten-Refresh wird das Letzte aufgerufene Objekt geladen
+		if (localStorage.getItem('LastObj') == null) {
+			//Objekt Speichern
+			let aktuellObjekt = {
+				ID: this.ID,
+				Name: this.Name,
+				Preis: this.Preis,
+				Kurzbeschreibung: this.Kurzbeschreibung,
+				Kategorie: this.Kategorie,
+				Link3D: this.Link3D,
+				Bewertung: this.Bewertung,
+			};
+			localStorage.setItem('LastObj', JSON.stringify(aktuellObjekt));
+		} else {
+			//Objekt Laden
+			let erg = JSON.parse(localStorage.getItem('LastObj'));
+			this.Name = erg.Name;
+			this.Preis = erg.Preis;
+			this.Kategorie = erg.Kategorie;
+			this.Kurzbeschreibung = erg.Kurzbeschreibung;
+			this.Link3D = erg.Link3D;
+			this.Bewertung = erg.Bewertung;
+		}
+	},
 };
 </script>
 
